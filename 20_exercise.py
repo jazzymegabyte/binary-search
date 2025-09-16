b1e537bb3af8cb73090209ae90e140ba12aa5108
jazzymegabyte <jazilatif6d@gmail.com>
2025-09-16 19:42:27 +0500

    initial and final commit

---
+++ 20_exercise.py
@@ -0,0 +1,22 @@
+# l = [1,2,4,6,8]
+# a = 3
+
+# while len(l) != 1:
+#     if a < l[len(l)//2]:
+#         l = l[0:len(l)//2]
+#     else:
+#         l = l[len(l)//2:]
+
+# print(a in l)
+        
+
+ordered_list = [1,2,4,6,8]
+a = 3
+print(len(ordered_list))
+ 
+
+
+
+
+
+
