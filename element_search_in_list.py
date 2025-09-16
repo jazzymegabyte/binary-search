666a3a51edcc4f327ebc56ed99ed9e687c02b24f
jazzymegabyte <jazilatif6d@gmail.com>
2025-09-16 19:46:27 +0500

    new recursive method addition

--- element_search_in_list.py
+++ element_search_in_list.py
@@ -1,22 +1,30 @@
-# l = [1,2,4,6,8]
-# a = 3
+# iterative method to binary search
 
-# while len(l) != 1:
-#     if a < l[len(l)//2]:
-#         l = l[0:len(l)//2]
-#     else:
-#         l = l[len(l)//2:]
+l = [1,2,4,6,8]
+a = 3
 
-# print(a in l)
+while len(l) != 1:
+    if a < l[len(l)//2]:
+        l = l[0:len(l)//2]
+    else:
+        l = l[len(l)//2:]
+
+print(a in l)
         
 
-ordered_list = [1,2,4,6,8]
-a = 3
-print(len(ordered_list))
- 
+#recursive method to binary search
 
 
+def binary_search_recursive(arr, target, low, high):
+    if low > high:
+        return -1
 
-
+    mid = (low + high) // 2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search_recursive(arr, target, mid + 1, high)
+    else:
+        return binary_search_recursive(arr, target, low, mid - 1)
 
 
